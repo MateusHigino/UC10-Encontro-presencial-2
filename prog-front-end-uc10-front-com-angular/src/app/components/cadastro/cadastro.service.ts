f8588b2b0db26b9ac165db64daf4a7d02623d633
MateusHigino <mateus.higino2003@gmail.com>
2021-12-04 02:16:19 -0300

    Meu primeiro commit

---
+++ prog-front-end-uc10-front-com-angular/src/app/components/cadastro/cadastro.service.ts
@@ -0,0 +1,58 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { EMPTY, Observable } from 'rxjs';
+import { Cadastro } from './cadastro.model';
+import { map, catchError } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CadastroService {
+
+  baseUrl = "http://localhost:3001/cadastro";
+
+  constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
+  
+  showMessege(msg: string,isError: boolean = false): void {
+    this.snackBar.open(msg,'X',{
+      duration: 6000,
+      verticalPosition: "bottom",
+      panelClass: isError ? ['errorMsg'] : ['successMsg']
+    });
+  }
+
+  create(cadastro : Cadastro):Observable<Cadastro>{
+    return this.http.post<Cadastro>(this.baseUrl,cadastro).pipe(
+    map((obj) => obj),
+    catchError(e => this.errorMsg(e))
+    )
+
+  }
+  
+  errorMsg(e: any): Observable<any>{
+   console.log(e);
+   this.showMessege(' Erro', true);
+   return EMPTY
+  }
+
+  read(): Observable<Cadastro[]> {
+    return this.http.get<Cadastro[]>(this.baseUrl);
+  }
+
+  readById(id: number): Observable<Cadastro> {
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.get<Cadastro>(url);
+  }
+
+  updateCadastro(cadastro: Cadastro): Observable<Cadastro> {
+    const url = `${this.baseUrl}/${cadastro.id}`;
+    return this.http.put<Cadastro>(url, cadastro);
+  }
+
+  deleteCadastro(id: number): Observable<Cadastro> {
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.delete<Cadastro>(url);
+  }
+
+}
